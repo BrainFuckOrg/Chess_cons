6ac4f0960989fe6562359985075bb996efe514b1
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-27 19:27:40 +0200

    Add castlingMethod

---
+++ ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Castling.cs
@@ -0,0 +1,85 @@
+using ConsoleApp3;
+using FiguresNameSpace;
+
+namespace Figure;
+
+
+
+public static class Castling
+{
+    private static CastleType[] WhiteCastle = { CastleType.Short, CastleType.Long };
+    private static CastleType[] BlackCastle = { CastleType.Short, CastleType.Long };
+    private static SByte[] BlackKing = { 7, 4 };
+    private static SByte[] WhiteKing = { 0, 4 };
+    
+    
+    public static Boolean CheckAndMove(CastleType type, Side side)
+    {
+        return side switch
+        {
+            Side.White => WhiteCastleMove(type),
+            Side.Black => BlackCastleMove(type),
+            _ => throw new ArgumentOutOfRangeException(nameof(side), side, null)
+        };
+    }
+
+    private static Boolean BlackCastleMove(CastleType type)
+    {
+        return type switch
+        {
+            CastleType.Short => BlackCastleMoveShort(),
+            CastleType.Long => BlackCastleMoveLong(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+
+    private static Boolean BlackCastleMoveLong()
+    {
+        SByte[] rookPos = { 7, 7 };
+        if (!BlackCastle.Contains(CastleType.Long)) return false;
+        if (GeneralFigureMethods.HasAnotherFigureInMoveLine(BlackKing, rookPos)) return false;
+        GeneralFigureMethods.MoveFigureFromTo(BlackKing, new SByte[] { 7, 6 });
+        GeneralFigureMethods.MoveFigureFromTo(rookPos, new SByte[] { 7, 5 });
+        return true;
+    }
+
+    private static Boolean BlackCastleMoveShort()
+    {        
+        SByte[] rookPos = { 7, 0 };
+        if (!BlackCastle.Contains(CastleType.Short)) return false;
+        if (GeneralFigureMethods.HasAnotherFigureInMoveLine(BlackKing, rookPos)) return false;
+        GeneralFigureMethods.MoveFigureFromTo(BlackKing, new SByte[] { 7, 2 });
+        GeneralFigureMethods.MoveFigureFromTo(rookPos, new SByte[] { 7, 3 });
+        return true;
+    }
+
+    private static Boolean WhiteCastleMove(CastleType type)
+    {
+        return type switch
+        {
+            CastleType.Short => WhiteCastleMoveShort(),
+            CastleType.Long => WhiteCastleMoveLong(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+
+    private static Boolean WhiteCastleMoveLong()
+    {        
+        SByte[] rookPos = { 0, 7 };
+        if (!WhiteCastle.Contains(CastleType.Long)) return false;
+        if (GeneralFigureMethods.HasAnotherFigureInMoveLine(WhiteKing, rookPos)) return false;
+        GeneralFigureMethods.MoveFigureFromTo(WhiteKing, new SByte[] { 0, 6 });
+        GeneralFigureMethods.MoveFigureFromTo(rookPos, new SByte[] { 0, 5 });
+        return true;
+    }
+
+    private static Boolean WhiteCastleMoveShort()
+    {        
+        SByte[] rookPos = { 7, 0 };
+        if (!WhiteCastle.Contains(CastleType.Short)) return false;
+        if (GeneralFigureMethods.HasAnotherFigureInMoveLine(BlackKing, rookPos)) return false;
+        GeneralFigureMethods.MoveFigureFromTo(BlackKing, new SByte[] { 7, 2 });
+        GeneralFigureMethods.MoveFigureFromTo(rookPos, new SByte[] { 7, 3 });
+        return true;
+    }
+}
