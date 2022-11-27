c8f36ca1fe90fc8d16d2f3887378d35a14aace18
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-27 20:47:06 +0200

    pawn can move and take figures

--- ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
+++ ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
@@ -1,4 +1,6 @@
 using ConsoleApp3;
+using FieldNameSpace;
+using FiguresNameSpace;
 
 namespace Figure;
 
@@ -7,7 +9,7 @@ public static class Pawn
     //TODO pawn -> Queen
     public static Boolean CheckAndMove(SByte[] from, SByte[] to)
     {
-        if (!isThisFigureMove(from, to)) return false;
+        if (!(isThisFigureMove(from, to) || isPawnTake(from, to))) return false;
         if (GeneralFigureMethods.HasAnotherThisColorFigure(from, to)) return false;
         if (GeneralFigureMethods.HasAnotherFigureInMoveLine(from, to)) return false;
         GeneralFigureMethods.MoveFigureFromTo(from,to);
@@ -16,7 +18,25 @@ public static class Pawn
 
     private static Boolean isThisFigureMove(sbyte[] from, sbyte[] to)
     {
-        return true;
-        
+        if (from[1] != to[1]) return false;
+        FigureNames[,] Table = Field.SingleField.Table;
+        return Table[from[0], from[1]] switch
+        {
+            FigureNames.PawnB => from[0] - to[0] == 1 || (from[0] == 6 && from[0] - to[0] == 2),
+            FigureNames.PawnW => from[0] - to[0] == -1 || (from[0] == 1 && from[0] - to[0] == -2),
+            _ => false
+        };
+    }
+    
+    private static Boolean isPawnTake(sbyte[] from, sbyte[] to)
+    {
+        FigureNames[,] Table = Field.SingleField.Table;
+        if (Table[to[0], to[1]] == FigureNames.Empty) return false;
+        return Table[from[0], from[1]] switch
+        {
+            FigureNames.PawnB => from[0] - to[0] == 1 && Math.Abs(from[1]-to[1])==1,
+            FigureNames.PawnW => from[0] - to[0] == -1 && Math.Abs(from[1]-to[1])==1,
+            _ => false
+        };
     }
 }
