c96653b964ae3dfa4f3b63e3dca9a88f613b7bcd
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-11-27 20:55:59 +0200

    pawn can transformate at the last line

--- ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
+++ ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
@@ -1,6 +1,7 @@
 using ConsoleApp3;
 using FieldNameSpace;
 using FiguresNameSpace;
+using GameLoopNameSpace;
 
 namespace Figure;
 
@@ -13,13 +14,15 @@ public static class Pawn
         if (GeneralFigureMethods.HasAnotherThisColorFigure(from, to)) return false;
         if (GeneralFigureMethods.HasAnotherFigureInMoveLine(from, to)) return false;
         GeneralFigureMethods.MoveFigureFromTo(from,to);
+        checkIfTheLastLine(to);
         return true;
     }
 
-    private static Boolean isThisFigureMove(sbyte[] from, sbyte[] to)
+    private static Boolean isThisFigureMove(SByte[] from, SByte[] to)
     {
         if (from[1] != to[1]) return false;
         FigureNames[,] Table = Field.SingleField.Table;
+        if (Table[to[0], to[1]] != FigureNames.Empty) return false;
         return Table[from[0], from[1]] switch
         {
             FigureNames.PawnB => from[0] - to[0] == 1 || (from[0] == 6 && from[0] - to[0] == 2),
@@ -28,7 +31,7 @@ public static class Pawn
         };
     }
     
-    private static Boolean isPawnTake(sbyte[] from, sbyte[] to)
+    private static Boolean isPawnTake(SByte[] from, SByte[] to)
     {
         FigureNames[,] Table = Field.SingleField.Table;
         if (Table[to[0], to[1]] == FigureNames.Empty) return false;
@@ -38,5 +41,13 @@ public static class Pawn
             FigureNames.PawnW => from[0] - to[0] == -1 && Math.Abs(from[1]-to[1])==1,
             _ => false
         };
+    }
+
+    private static void checkIfTheLastLine(SByte[] to)
+    {
+        if (!(to[0]==0 || to[0]==7)) return;
+        
+        FigureNames[,] Table = Field.SingleField.Table;
+        Table[to[0],to[1]] = GameLoop.AskPawnTransformation();
     }
 }
