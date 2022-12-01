280a5631e02b847a79f165f4fde3ef3a67d8a14a
KosenkoAlexander <alexandkosenko@gmail.com>
2022-12-01 19:37:57 +0200

    GetLastMove() fixed

--- ConsoleApp3/Chess/Figures/Figures.cs
+++ ConsoleApp3/Chess/Figures/Figures.cs
@@ -1,3 +1,5 @@
+using Figure;
+
 namespace FiguresNameSpace;
 
 public enum FigureNames
@@ -33,4 +35,24 @@ public static class Figure
 {
     public static readonly FigureNames[] WhiteFigures = { FigureNames.BishopW, FigureNames.KingW, FigureNames.KnightW, FigureNames.PawnW, FigureNames.QueenW, FigureNames.RookW };
     public static readonly FigureNames[] BlackFigures = { FigureNames.BishopB, FigureNames.KingB, FigureNames.KnightB, FigureNames.PawnB, FigureNames.QueenB, FigureNames.RookB };
+}
+
+public static class CanHit
+{
+    public static Boolean CanHitCell(SByte[] cell, Side color)
+    {
+        throw new NotImplementedException();
+        /*for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+        {
+            if (color == Side.White)
+            {
+                return FieldNameSpace.Field.SingleField.Table[i,j] switch
+                {
+                    FigureNames.BishopW=>false
+                }
+            }
+        }
+        */
+    }
 }
