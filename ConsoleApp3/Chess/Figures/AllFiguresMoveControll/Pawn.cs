9f80c41188086acaa782271666e5bdb8d506a5d6
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-01 19:33:41 +0200

    add en passant

--- ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
+++ ConsoleApp3/Chess/Figures/AllFiguresMoveControll/Pawn.cs
@@ -11,6 +11,7 @@ public static class Pawn
     {
         if (!PawnCanMove(from, to)) return false;
         GeneralFigureMethods.MoveFigureFromTo(from,to);
+        if(isEnPassant(from,to)) removePassantPawn();
         checkIfTheLastLine(to);
         return true;
     }
@@ -37,6 +38,7 @@ public static class Pawn
     
     private static Boolean isPawnTake(SByte[] from, SByte[] to)
     {
+        if (isEnPassant(from, to)) return true;
         FigureNames[,] Table = Field.SingleField.Table;
         if (Table[to[0], to[1]] == FigureNames.Empty) return false;
         return Table[from[0], from[1]] switch
@@ -45,6 +47,53 @@ public static class Pawn
             FigureNames.PawnW => from[0] - to[0] == -1 && Math.Abs(from[1]-to[1])==1,
             _ => false
         };
+    }
+
+    private static Boolean isEnPassant(SByte[] from, SByte[] to)
+    {
+        FigureNames[,] Table = Field.SingleField.Table;
+        if (Table[to[0], to[1]] != FigureNames.Empty) return false;
+        if (Math.Abs(from[1] - to[1]) != 1) return false;
+        if (FiguresNameSpace.Figure.BlackFigures.Contains(Table[from[0],from[1]]))
+        {
+            return BlackEnPassant(from, to);
+        }
+
+        return WhiteEnPassant(from, to);
+    }
+
+    private static void removePassantPawn()
+    {
+        FigureNames[,] Table = Field.SingleField.Table;
+        FigureNames figure;
+        SByte[] startCoordinates;
+        SByte[] endCoordinates;
+        JournalNamespace.Journal.GetLastMove(out figure, out startCoordinates, out endCoordinates);
+        Table[endCoordinates[0], endCoordinates[1]] = FigureNames.Empty;
+    }
+
+    private static Boolean WhiteEnPassant(SByte[] from, SByte[] to)
+    {
+        FigureNames figure;
+        SByte[] startCoordinates;
+        SByte[] endCoordinates;
+        JournalNamespace.Journal.GetLastMove(out figure, out startCoordinates, out endCoordinates);
+        if (figure != FigureNames.PawnB) return false;
+        if (startCoordinates[0] == 6 && endCoordinates[0] == 4 && startCoordinates[1] == to[1] &&
+            to[0] == 5) return true;
+        return false;
+    }
+
+    private static Boolean BlackEnPassant(SByte[] from, SByte[] to)
+    {        
+        FigureNames figure;
+        SByte[] startCoordinates;
+        SByte[] endCoordinates;
+        JournalNamespace.Journal.GetLastMove(out figure, out startCoordinates, out endCoordinates);
+        if (figure != FigureNames.PawnW) return false;
+        if (startCoordinates[0] == 1 && endCoordinates[0] == 3 && startCoordinates[1] == to[1] &&
+            to[0] == 2) return true;
+        return false;
     }
 
     private static void checkIfTheLastLine(SByte[] to)
