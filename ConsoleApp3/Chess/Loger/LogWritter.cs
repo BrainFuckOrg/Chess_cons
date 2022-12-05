9593cc958df05e9f19940fad777bfb72882c2a41
MaksymShkarupylo <shkarupilo2005@gmail.com>
2022-12-05 18:29:55 +0200

    create logger

---
+++ ConsoleApp3/Chess/Loger/LogWritter.cs
@@ -0,0 +1,20 @@
+namespace ConsoleApp3.Loger;
+
+public static class LogWritter
+{
+    private static String path1 = "LogInfo";
+    
+    static LogWritter()
+    {
+        FileStream fs = new FileStream(path1, FileMode.OpenOrCreate);
+        fs.Close();
+        Logging("________________________________");
+    }
+
+    public static void Logging(String info)
+    {
+        StreamWriter sw = new StreamWriter(path1, false);
+        sw.WriteLine(info);
+        sw.Close();
+    }
+}
